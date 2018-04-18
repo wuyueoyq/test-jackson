c61b0adb60bb8d776a391a2330cc030444e69839
wuyueoyq <37974017+wuyueoyq@users.noreply.github.com>
2018-04-18 22:39:59 +0900

    Initial commit

---
+++ src/main/java/com/sample/Car.java
@@ -0,0 +1,20 @@
+package com.sample;
+
+public class Car {
+
+	private String color;
+	private String type;
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+}
